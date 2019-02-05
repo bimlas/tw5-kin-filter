bdddeba2cfd21d2570752cf0cbcc766667833259
bimlas <bimbalaszlo@gmail.com>
2019-02-05 23:40:20 +0100

    Add tests

---
+++ editions/test/tiddlers/tests/test-filters.js
@@ -0,0 +1,59 @@
+/*\
+title: test-filters.js
+type: application/javascript
+tags: [[$:/tags/test-spec]]
+
+Tests the filtering mechanism.
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+describe("Filter tests", function() {
+
+	it("should handle the kin operator",function() {
+		var wiki = new $tw.Wiki();
+		wiki.addTiddler({
+			title: "A",
+			tags: ["B"],
+			list: "C"
+		});
+		wiki.addTiddler({
+			title: "B",
+			tags: ["C"],
+		});
+		wiki.addTiddler({
+			title: "C",
+			tags: ["A"],
+			list: ["E"]
+		});
+		wiki.addTiddler({
+			title: "D",
+			tags: ["B"],
+		});
+		wiki.addTiddler({
+			title: "E",
+			tags: ["D"],
+		});
+		wiki.addTiddler({
+			title: "F",
+			tags: ["E"],
+		});
+		wiki.addTiddler({
+			title: "G",
+			tags: ["D"],
+		});
+
+		expect(wiki.filterTiddlers("[kin[A]sort[title]]").join(",")).toBe("A,B,C,D,E,F,G");
+		expect(wiki.filterTiddlers("[kin::from[A]sort[title]]").join(",")).toBe("A,B,C");
+		expect(wiki.filterTiddlers("[kin::to[A]sort[title]]").join(",")).toBe("A,B,C,D,E,F,G");
+		expect(wiki.filterTiddlers("[kin[A]!kin::to[D]sort[title]]").join(",")).toBe("A,B,C");
+		expect(wiki.filterTiddlers("[kin::from:2[F]sort[title]]").join(",")).toBe("D,E,F");
+		expect(wiki.filterTiddlers("[kin:list[C]]").join(",")).toBe("A,C,E");
+	});
+});
+
+})();
