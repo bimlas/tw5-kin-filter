1213192883f2b31900c3084442f5ac4aadddf0bf
bimlas <bimbalaszlo@gmail.com>
2024-02-12 21:50:07 +0100

    Add more performance tests

--- tests/tiddlers/test-performance.js
+++ tests/tiddlers/test-performance.js
@@ -15,22 +15,30 @@ Plugin performance tests
 if($tw.wiki.getTiddler("HelloThere")) {
 	describe("Performance tests", function() {
 
-		function timeOf(filter) {
+		function stats(filter) {
 			$tw.wiki.clearGlobalCache();
 			var start = Date.now();
-			$tw.wiki.filterTiddlers(filter);
-			return Date.now() - start;
+			var results = $tw.wiki.filterTiddlers(filter);
+			return {time: Date.now() - start, records: results.length};
 		}
 
 		it("works",function() {
 			const generators = [
 				"[kin[TableOfContents]]",
+				"[kin:::2[TableOfContents]]",
+				"[kin[Filters]]",
+				"[kin::to:2[Filters]]",
+				"[kin:list[$:/tags/SideBarSegment]]",
 			];
 			const filters = [
 				"[all[tiddlers+shadows]kin[TableOfContents]]",
+				"[all[tiddlers+shadows]kin:::2[TableOfContents]]",
+				"[all[tiddlers+shadows]kin[Filters]]",
+				"[all[tiddlers+shadows]kin::to:2[Filters]]",
+				"[all[tiddlers+shadows]kin:list[$:/tags/SideBarSegment]]",
 			];
 			var results = [...generators, ...filters].reduce(function (results, filter) {
-				results[filter] = timeOf(filter);
+				results[filter] = stats(filter);
 				return results;
 			}, {});
 			console.table(results);
